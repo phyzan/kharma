73d193422598a5fe189d18064f27b7cfac43dfe7
senpy <fivoszan@gmail.com>
2026-07-10 15:16:17 +0300

    Fixed collapsar initialization. TODO: r_out should be provided in cm in the parameter file

--- kharma/prob/collapsar.hpp
+++ kharma/prob/collapsar.hpp
@@ -18,7 +18,37 @@ constexpr Real MSUN = 1.989e33;    // Solar mass in grams
 constexpr Real GNEWT = 6.674e-8;   // G in cm³/g/s²
 constexpr Real CL = 2.998e10;      // c in cm/s
     
-} // namespace constants
+} // namespace kharma_constants
+
+
+template<typename T, typename Callable>
+T bisect(Callable&& f, const T& lower, const T& upper, const T& atol){
+    T err = 2*atol+1;
+    T a = lower;
+    T b = upper;
+    T c = a;
+    T fm;
+
+    assert((f(a) * f(b) <= 0) && "Root not bracketed" );
+    
+    while (err > atol){
+        c = (a+b)/2;
+        if (c == a || c == b){
+            // reached machine precision limit, return the best guess so far
+            break;
+        }
+        fm = f(c);
+        if (f(a) * fm  > 0){
+            a = c;
+        }
+        else{
+            b = c;
+        }
+        err = abs<T>(fm);
+    }
+
+    return b;
+}
 
 class DeviceInterpolator1D {
 
@@ -100,9 +130,18 @@ inline TaskStatus InitializeCollapsar(std::shared_ptr<MeshBlockData<Real>>& rc, 
     const Real M_SUN = kharma_constants::MSUN;    // Solar mass in grams
     const Real G = kharma_constants::GNEWT;   // G in cm³/g/s²
     const Real c = kharma_constants::CL;      // light speed in cm/s
-    const Real time_unit = G * M_BH * M_SUN / (c * c * c); // in seconds
-    const Real length_unit = G * M_BH * M_SUN / (c * c); // in cm
+    const Real mass_unit = M_BH * M_SUN; // in grams
+    const Real time_unit = G * mass_unit / (c * c * c); // in seconds
+    const Real length_unit = G * mass_unit / (c * c); // in cm
     const Real pressure_unit = density_unit * c * c; // in erg/cm³
+
+
+    // set physical scales
+    pin->SetReal("collapsar", "units.length", length_unit);
+    pin->SetReal("collapsar", "units.time", time_unit);
+    pin->SetReal("collapsar", "units.density", density_unit);
+    pin->SetReal("collapsar", "units.pressure", pressure_unit);
+    pin->SetReal("collapsar", "units.magnetic_field", c*m::sqrt(4.0*M_PI*density_unit));
 
     
     GridScalar rho = rc->Get("prims.rho").data;
@@ -120,29 +159,46 @@ inline TaskStatus InitializeCollapsar(std::shared_ptr<MeshBlockData<Real>>& rc, 
 
 
     // Get the data of each column in the file
-    std::vector<Real> r, rho_r, Omega_r, u_r;
+    std::vector<Real> r, rho_r, Omega_r, u_r, M_r;
     csv::CSVReader reader(file);
     for (csv::CSVRow& row : reader) {
         Real r_val = row["r"].get<Real>() / length_unit; // Convert to code units
         Real rho_val = row["rho_r"].get<Real>() / density_unit;
         Real Omega_val = row["Omega_r"].get<Real>() * time_unit; // Convert to code units
         Real P_val = row["P_r"].get<Real>() / pressure_unit;
+        Real M_val = row["M_r"].get<Real>() / mass_unit; // Convert to code units
         r.push_back(r_val);
         rho_r.push_back(rho_val);
         Omega_r.push_back(Omega_val);
         u_r.push_back(P_val/(gam - 1.0));
+        M_r.push_back(M_val);
     }
+
+    if (M_r.back() < 1.0) {
+        PARTHENON_THROW("The mass of the star (" + std::to_string(M_r.back()*mass_unit/M_SUN) + " M_sun) is less than the requested black hole mass (" + std::to_string(M_BH) + " M_sun).");
+    }
+
     const Real r_max = r.back();
     const Real r_min = r.front();
-    std::cout << "rmin = " << r_min << std::endl;
-    std::cout << "rmax = " << r_max << std::endl;
-    std::cout << "length unit = " << length_unit << std::endl;
+
     
     DeviceInterpolator1D rho_interp(r, rho_r);
     DeviceInterpolator1D Omega_interp(r, Omega_r);
     DeviceInterpolator1D u_interp(r, u_r);
+    DeviceInterpolator1D M_interp(r, M_r);
 
-    // Define unit quantities to convert to code units
+    // Find the radius where the mass of the star is equal to the requested black hole mass, or equivalently, the core radius where the mass enclosed
+    Real r_core = bisect([&](Real r) { return M_interp(r) - 1.0; },r_min, r_max, 0.0);
+
+
+    if (MPIRank0() && pmb->gid == 0){
+        std::cout << "rmin = " << r_min << " (in code units)" << std::endl;
+        std::cout << "rmax = " << r_max << " (in code units)" << std::endl;
+        std::cout << "length unit = " << length_unit << " cm" << std::endl;
+        std::cout << "Core radius = " << r_core << " (in code units)" << std::endl;
+        std::cout << "Core radius = " << r_core*length_unit << " cm" << std::endl;
+        std::cout << "Mass of the star = " << M_interp(r_max) * mass_unit / M_SUN << " M_sun" << std::endl;
+    }
 
 
     IndexDomain domain = IndexDomain::interior;
@@ -154,7 +210,7 @@ inline TaskStatus InitializeCollapsar(std::shared_ptr<MeshBlockData<Real>>& rc, 
             Real X[GR_DIM];
             GR.coord_embed(k, j, i, Loci::center, X);
             const GReal r = X[1];
-            if (r <= r_max && r >= r_min) {
+            if (r <= r_max && r >= r_core) {
 
                 Real Omega = Omega_interp(r);
                 Real g_tt = GR.gcov(Loci::center, j, i, 0, 0);
@@ -175,13 +231,6 @@ inline TaskStatus InitializeCollapsar(std::shared_ptr<MeshBlockData<Real>>& rc, 
                 uvec(2, k, j, i) = u_prim[2];
             }
         });
-
-    // set physical scales
-    pin->SetReal("collapsar", "units.length", length_unit);
-    pin->SetReal("collapsar", "units.time", time_unit);
-    pin->SetReal("collapsar", "units.density", density_unit);
-    pin->SetReal("collapsar", "units.pressure", pressure_unit);
-    pin->SetReal("collapsar", "units.magnetic_field", c*m::sqrt(4.0*M_PI*density_unit));
 
     Floors::ApplyInitialFloors(pin, rc.get(), IndexDomain::interior);
     return TaskStatus::complete;
